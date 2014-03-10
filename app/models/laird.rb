423e38f56bbfe6db79cd22ab3ce00a8571073475
Alexandr <xandrck@gmail.com>
2014-03-10 11:56:35 +0200

    fix models structure (spaces)

--- app/models/laird.rb
+++ app/models/laird.rb
@@ -1,9 +1,11 @@
-class Laird < ActiveRecord::Base
-belongs_to :category
+class Laird < User
+  
+  belongs_to :category
 
-mount_uploader :image, SoulImageUploader
-def to_param
-	"#{id}-#{Russian::transliterate(title).parameterize}"		
-end
+  mount_uploader :image, SoulImageUploader
+
+  def to_param
+    "#{id}-#{Russian::transliterate(title).parameterize}"
+  end
 
 end
--- app/models/soul.rb
+++ app/models/soul.rb
@@ -1,14 +1,15 @@
-class Soul < ActiveRecord::Base
+class Soul < User
+  
 	belongs_to :category
 
  	validates_presence_of :name, :category_id
  	validates_length_of :name, maximum: 200
  	validates_numericality_of :price, greater_that_or_equal_to: 0
 
-mount_uploader :image, SoulImageUploader
+  mount_uploader :image, SoulImageUploader
 
-def to_param
-	"#{id}-#{Russian::transliterate(name).parameterize}"		
-end
+  def to_param
+    "#{id}-#{Russian::transliterate(name).parameterize}"
+  end
 
 end
