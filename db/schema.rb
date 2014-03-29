534ac42dfb81c5dd3a0934ce483fd900c7139e5e
Alexandr <xandrck@gmail.com>
2014-03-29 04:41:16 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310093338) do
+ActiveRecord::Schema.define(version: 20140329014009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,10 @@ ActiveRecord::Schema.define(version: 20140310093338) do
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "auth_token"
+    t.string   "confirm_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   create_table "work_as", force: true do |t|
