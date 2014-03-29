48184b2fb4c79ba2e3837a6adeb0847cfffd87e0
Alexandr <xandrck@gmail.com>
2014-03-29 04:42:07 +0200

    added token methods (tokens generation on user creation and password reset)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,20 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+  before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
 
   #def to_param
   #  "#{id}-#{Russian::transliterate(name).parameterize}"
