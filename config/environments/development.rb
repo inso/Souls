4437c7540bba0df9ecc8909d45452ad04e36d043
Alexandr <xandrck@gmail.com>
2014-03-30 09:24:44 +0300

    removed host from views and set it in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Souls::Application.configure do
   config.assets.debug = true
 
   #Action_mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
