a8f347ac1bcc2f3b62fa866b45e6cf3f28fc7d7f
Alexandr <xandrck@gmail.com>
2014-03-31 17:57:29 +0300

    uncommented resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Souls::Application.routes.draw do
   devise_for :users
   #devise_for :admin_users, ActiveAdmin::Devise.config
   #ActiveAdmin.routes(self)
-  #resources :souls, :lairds, :pages, :work_as
+  resources :souls, :lairds, :pages, :work_as
   
   #get "souls/index"
   #get "souls/show"
