9e1a0bc8b04ef9a871d98ad3460b6fa63f9941fb
Alexandr <xandrck@gmail.com>
2014-03-31 18:06:09 +0300

    now user type will saved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user
+  protected
 
-  private
-
-  def current_user
-    @current_user ||= User.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]
-    rescue ActiveRecord::RecordNotFound
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :type) }
   end
 
 end
