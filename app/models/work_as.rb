d0646cfa6a47c207ea3f91588b0f3250e689156f
Alexandr <xandrck@gmail.com>
2014-04-01 13:06:12 +0300

    added validates_presence_of works atributes on create and update actions

--- app/models/work_as.rb
+++ app/models/work_as.rb
@@ -3,6 +3,8 @@ class WorkAs < ActiveRecord::Base
   belongs_to :laird
   belongs_to :category
 
+  validates_presence_of :name, :category_id, :old, :price, :describe, :status, on: :create, on: :update
+
   mount_uploader :image, SoulImageUploader
 
   def to_param
