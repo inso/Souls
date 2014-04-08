afb673ea286d66331cecc16b4d4cfaf98ecce8ab
Alexandr <xandrck@gmail.com>
2014-04-08 15:22:14 +0300

    add validations of data for models

--- app/models/soul.rb
+++ app/models/soul.rb
@@ -1,3 +1,8 @@
 class Soul < User
 
+  validates_presence_of :second_name, :pname, :old, on: :update
+  validates :second_name, :pname, format: { with: /\A[a-zA-Z]+\z/, message: "only letters are allowed" }, length: { minimum: 4 }
+  validates_numericality_of :old, on: :update, greater_than_or_equal_to: 16, less_than: 70, only_integer: true
+  validates_numericality_of :price, on: :update, greater_than: 10
+
 end
--- app/models/work_as.rb
+++ app/models/work_as.rb
@@ -3,7 +3,9 @@ class WorkAs < ActiveRecord::Base
   belongs_to :laird
   belongs_to :category
 
-  validates_presence_of :name, :category_id, :old, :price, :describe, :status, on: :create, on: :update
+  validates_presence_of :name, :category_id, :price, :describe, on: :create, on: :update
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "only letters are allowed" }, length: { minimum: 4 }
+  validates_numericality_of :price, on: :update, greater_than: 10
 
   mount_uploader :image, SoulImageUploader
 
